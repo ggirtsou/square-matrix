839b6b5b196d3a3b35a70ba0542d8b4475e16057
George Gkirtsou <ggirtsou@users.noreply.github.com>
2016-08-20 19:28:13 +0100

    Initial commit
    - Project scaffolding

---
+++ src/Gkirtsou/SquareMatrix.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Gkirtsou;
+
+/**
+ * Class SquareMatrix
+ * @package Gkirtsou
+ */
+class SquareMatrix
+{
+    public function __construct(array $data)
+    {
+        // @todo
+    }
+}
---
+++ tests/Gkirtsou/SquareMatrixTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Gkirtsou\SquareMatrix;
+
+class SquareMatrixTest extends \PHPUnit_Framework_TestCase
+{
+    public function testClass()
+    {
+        $class = new SquareMatrix([]);
+        // @todo
+    }
+}
